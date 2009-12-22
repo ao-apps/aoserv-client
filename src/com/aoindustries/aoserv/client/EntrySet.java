9b4cbe6949812f6ab775eb9228dbfac04d2ca51c
Dan Armstrong <dan@aoindustries.com>
2009-12-22 14:03:24 +0000

    Working on AOServ version 2.0

--- src/com/aoindustries/aoserv/client/EntrySet.java
+++ src/com/aoindustries/aoserv/client/EntrySet.java
@@ -7,9 +7,8 @@ package com.aoindustries.aoserv.client;
  */
 import java.util.AbstractSet;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.NoSuchElementException;
+import java.util.Set;
 
 /**
  * An entry set that may be used by any of the various services.
@@ -18,10 +17,10 @@ import java.util.NoSuchElementException;
  */
 final class EntrySet<K extends Comparable<K>,V extends AOServObject<K,V>> extends AbstractSet<Map.Entry<K,V>> {
 
-    private List<V> objs;
+    private Set<V> objs;
 
-    EntrySet(List<V> objs) {
-        this.objs=objs;
+    EntrySet(Set<V> objs) {
+        this.objs = objs;
     }
 
     public int size() {
@@ -30,39 +29,31 @@ final class EntrySet<K extends Comparable<K>,V extends AOServObject<K,V>> extend
 
     public Iterator<Map.Entry<K,V>> iterator() {
         return new Iterator<Map.Entry<K,V>>() {
-
-            private int cursor=0;
-
-            private int lastRet=-1;
+            final Iterator<V> iter = objs.iterator();
 
             public void remove() {
                 throw new UnsupportedOperationException();
             }
 
             public boolean hasNext() {
-                return cursor < objs.size();
+                return iter.hasNext();
             }
 
             public Map.Entry<K,V> next() {
-                try {
-                    final V value=objs.get(cursor);
-                    final K key=value.getKey();
-                    Map.Entry<K,V> next = new Map.Entry<K,V>() {
-                        public V setValue(V value) {
-                            throw new UnsupportedOperationException();
-                        }
-                        public V getValue() {
-                            return value;
-                        }
-                        public K getKey() {
-                            return key;
-                        }
-                    };
-                    lastRet = cursor++;
-                    return next;
-                } catch(IndexOutOfBoundsException e) {
-                    throw new NoSuchElementException();
-                }
+                final V value = iter.next();
+                final K key = value.getKey();
+                Map.Entry<K,V> next = new Map.Entry<K,V>() {
+                    public V setValue(V value) {
+                        throw new UnsupportedOperationException();
+                    }
+                    public V getValue() {
+                        return value;
+                    }
+                    public K getKey() {
+                        return key;
+                    }
+                };
+                return next;
             }
         };
     }
