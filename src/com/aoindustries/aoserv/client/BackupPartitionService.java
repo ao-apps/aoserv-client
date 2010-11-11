8453952e89f4b371a58bbd351f6bdee130302d52
Dan Armstrong <dan@aoindustries.com>
2010-11-11 07:40:45 +0000

    Using wildcard imports for intra-project packages.

--- src/com/aoindustries/aoserv/client/BackupPartitionService.java
+++ src/com/aoindustries/aoserv/client/BackupPartitionService.java
@@ -1,14 +1,11 @@
-package com.aoindustries.aoserv.client;
-
 /*
  * Copyright 2002-2010 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
+package com.aoindustries.aoserv.client;
 
 /**
- * @see  BackupPartitionTable
- *
  * @author  AO Industries, Inc.
  */
 @ServiceAnnotation(ServiceName.backup_partitions)
