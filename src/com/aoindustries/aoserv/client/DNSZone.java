56a460b81e639a36ac2bc3bad56e4ffc12abf6e2
Dan Armstrong <dan@aoindustries.com>
2012-05-07 19:44:11 +0000

    Breaking up long lines in zone files.

--- src/com/aoindustries/aoserv/client/DNSZone.java
+++ src/com/aoindustries/aoserv/client/DNSZone.java
@@ -15,7 +15,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
-import java.util.Locale;
 
 /**
  * A <code>DNSZone</code> is one domain hosted in the name servers.  It can have
@@ -38,6 +37,11 @@ final public class DNSZone extends CachedObjectStringKey<DNSZone> implements Rem
     static final String COLUMN_ZONE_name= "zone";
     
     public static final int DEFAULT_TTL = 43200;
+
+    /**
+     * The maximum number of characters allowed in a line.
+     */
+    public static final int MAX_LINE_LENGTH = 255;
 
     /**
      * The zone that is in charge of the API.
@@ -192,56 +196,96 @@ final public class DNSZone extends CachedObjectStringKey<DNSZone> implements Rem
         return pkey.length()>13 && pkey.substring(pkey.length()-13).equals(".in-addr.arpa");
     }
 
-    private static void printRecord(PrintWriter out, String domain, int ttl, int recordTtl, String type, int mx, String destination) {
-        out.print(domain);
+    /**
+     * Checks that the line is not too long, prints the line, and clears the buffer.
+     */
+    private static void printLine(StringBuilder line, PrintWriter out) {
+        if(line.length()>MAX_LINE_LENGTH) throw new IllegalStateException("Line too long: "+line);
+        out.print(line);
+        out.print('\n');
+        line.setLength(0);
+    }
+
+    private static void printRecord(String linePrefix, StringBuilder line, PrintWriter out, String domain, int ttl, int recordTtl, String type, int mx, String destination) {
+        line.append(linePrefix);
+        line.append(domain);
         int count=Math.max(1, 24-domain.length());
-        for(int d=0;d<count;d++) out.print(' ');
+        for(int d=0;d<count;d++) line.append(' ');
         if (recordTtl!=DNSRecord.NO_TTL) {
             String s=String.valueOf(recordTtl);
-            out.print(s);
+            line.append(s);
             count=Math.max(1, 24-s.length());
         } else {
             String s=String.valueOf(ttl);
-            out.print(s);
+            line.append(s);
             count=Math.max(1, 24-s.length());
         }
-        for(int d=0;d<count;d++) out.print(' ');
-        out.print("IN   ");
-        out.print(type);
+        for(int d=0;d<count;d++) line.append(' ');
+        line.append("IN   ");
+        line.append(type);
         count=Math.max(1, 8-type.length());
-        for(int d=0;d<count;d++) out.print(' ');
+        for(int d=0;d<count;d++) line.append(' ');
         if(mx!=DNSRecord.NO_MX_PRIORITY) {
-            out.print(mx);
-            out.print(' ');
+            line.append(mx);
+            line.append(' ');
         }
         if(type.equals(DNSType.TXT)) {
-            // Double-quote TXT types and filter " and anything < (space) or >= (char)0x7f from the destination
-            out.print('"');
-            for(int d=0, dlen=destination.length(); d<dlen; d++) {
-                char ch = destination.charAt(d);
-                if(
-                    ch!='"'
-                    && ch>=' '
-                    && ch<(char)0x7f
-                ) out.print(ch);
+            int oneLineLength = line.length() + 1 + destination.length() + 1;
+            if(oneLineLength<MAX_LINE_LENGTH) {
+                // Double-quote TXT types and filter " and anything < (space) or >= (char)0x7f from the destination
+                line.append('"');
+                for(int d=0, dlen=destination.length(); d<dlen; d++) {
+                    char ch = destination.charAt(d);
+                    if(
+                        ch!='"'
+                        && ch>=' '
+                        && ch<(char)0x7f
+                    ) line.append(ch);
+                }
+                line.append('"');
+                printLine(line, out);
+            } else {
+                // Begin parenthesis
+                line.append('(');
+                printLine(line, out);
+                // Double-quote TXT types and filter " and anything < (space) or >= (char)0x7f from the destination
+                final int charsPerLine = MAX_LINE_LENGTH-(linePrefix.length()+2);
+                for(int lineStart=0; lineStart<destination.length(); lineStart+=charsPerLine) {
+                    line.append(linePrefix);
+                    line.append('"');
+                    int lineEnd=Math.min(destination.length(), lineStart+charsPerLine);
+                    for(int d=lineStart; d<lineEnd; d++) {
+                        char ch = destination.charAt(d);
+                        if(
+                            ch!='"'
+                            && ch>=' '
+                            && ch<(char)0x7f
+                        ) line.append(ch);
+                    }
+                    line.append('"');
+                    printLine(line, out);
+                }
+                // End parenthesis
+                line.append(linePrefix);
+                line.append(')');
+                printLine(line, out);
             }
-            out.print('"');
         } else {
-            out.print(destination);
+            line.append(destination);
+            printLine(line, out);
         }
     }
 
     public void printZoneFile(PrintWriter out) throws SQLException, IOException {
+        StringBuilder line = new StringBuilder(); // Buffers each line to ensure not too long
 	List<DNSRecord> records=getDNSRecords();
-	out.print("$TTL    ");
-        out.print(ttl);
-        out.print('\n');
+	line.append("$TTL    ").append(ttl);
+        printLine(line, out);
 	if(!isArpa()) {
-            out.print("$ORIGIN "); out.print(pkey); out.print("\n");
+            line.append("$ORIGIN ").append(pkey);
+            printLine(line, out);
 	}
-	out.print("@                       ");
-        out.print(ttl);
-        out.print(" IN   SOA     ");
+	line.append("@                       ").append(ttl).append(" IN   SOA     ");
         // Find the first nameserver
         DNSRecord firstNS=null;
         for(DNSRecord record : records) {
@@ -250,21 +294,29 @@ final public class DNSZone extends CachedObjectStringKey<DNSZone> implements Rem
                 break;
             }
         }
-        out.print(firstNS==null ? "ns1.aoindustries.com." : firstNS.getDestination());
-        out.print("   "); out.print(hostmaster); out.print(" (\n"
-                + "                                "); out.print(serial); out.print(" ; serial\n"
-                + "                                3600    ; refresh\n"
-                + "                                600     ; retry\n"
-                + "                                1814400 ; expiry\n"
-                + "                                300     ; minimum\n"
-                + "                                )\n");
+        line.append(firstNS==null ? "ns1.aoindustries.com." : firstNS.getDestination());
+        line.append("   ").append(hostmaster).append(" (");
+        printLine(line, out);
+        line.append("                                ").append(serial).append(" ; serial");
+        printLine(line, out);
+        line.append("                                3600    ; refresh");
+        printLine(line, out);
+        line.append("                                600     ; retry");
+        printLine(line, out);
+        line.append("                                1814400 ; expiry");
+        printLine(line, out);
+        line.append("                                300     ; minimum");
+        printLine(line, out);
+        line.append("                                )");
+        printLine(line, out);
         if(firstNS==null) {
             // Add the default nameservers because named will refuse to start without them
-            out.print("; No name servers configured, using the defaults\n");
-            printRecord(out, "@", ttl, DNSRecord.NO_TTL, DNSType.NS, DNSRecord.NO_MX_PRIORITY, "ns1.aoindustries.com."); out.print('\n');
-            printRecord(out, "@", ttl, DNSRecord.NO_TTL, DNSType.NS, DNSRecord.NO_MX_PRIORITY, "ns2.aoindustries.com."); out.print('\n');
-            printRecord(out, "@", ttl, DNSRecord.NO_TTL, DNSType.NS, DNSRecord.NO_MX_PRIORITY, "ns3.aoindustries.com."); out.print('\n');
-            printRecord(out, "@", ttl, DNSRecord.NO_TTL, DNSType.NS, DNSRecord.NO_MX_PRIORITY, "ns4.aoindustries.com."); out.print('\n');
+            line.append("; No name servers configured, using the defaults");
+            printLine(line, out);
+            printRecord("", line, out, "@", ttl, DNSRecord.NO_TTL, DNSType.NS, DNSRecord.NO_MX_PRIORITY, "ns1.aoindustries.com.");
+            printRecord("", line, out, "@", ttl, DNSRecord.NO_TTL, DNSType.NS, DNSRecord.NO_MX_PRIORITY, "ns2.aoindustries.com.");
+            printRecord("", line, out, "@", ttl, DNSRecord.NO_TTL, DNSType.NS, DNSRecord.NO_MX_PRIORITY, "ns3.aoindustries.com.");
+            printRecord("", line, out, "@", ttl, DNSRecord.NO_TTL, DNSType.NS, DNSRecord.NO_MX_PRIORITY, "ns4.aoindustries.com.");
         }
 	int len=records.size();
 	for(int c=0;c<len;c++) {
@@ -276,8 +328,17 @@ final public class DNSZone extends CachedObjectStringKey<DNSZone> implements Rem
                     break;
                 }
             }
-            if(hasConflictAbove) out.print("; Disabled due to conflict: ");
-            printRecord(out, record.domain, ttl, record.ttl, record.type, record.mx_priority, record.destination);
+            printRecord(
+                hasConflictAbove ? "; Disabled due to conflict: " : "",
+                line,
+                out,
+                record.domain,
+                ttl,
+                record.ttl,
+                record.type,
+                record.mx_priority,
+                record.destination
+            );
             // Allow the first one when there is a conflict
             if(!hasConflictAbove) {
                 boolean hasConflictBelow = false;
@@ -287,9 +348,11 @@ final public class DNSZone extends CachedObjectStringKey<DNSZone> implements Rem
                         break;
                     }
                 }
-                if(hasConflictBelow) out.print(" ; Some records below have been disabled due to conflict");
+                if(hasConflictBelow) {
+                    line.append("; Some records below have been disabled due to conflict with previous record");
+                    printLine(line, out);
+                }
             }
-            out.print('\n');
 	}
     }
 
