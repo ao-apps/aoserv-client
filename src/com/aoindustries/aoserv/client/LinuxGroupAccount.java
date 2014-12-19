0baea4bc58113cea5381a4952d49c9fc884c10ff
Dan Armstrong <dan@aoindustries.com>
2014-12-19 21:56:09 +0000

    Routine tasks.

--- src/com/aoindustries/aoserv/client/LinuxGroupAccount.java
+++ src/com/aoindustries/aoserv/client/LinuxGroupAccount.java
@@ -1,14 +1,17 @@
-package com.aoindustries.aoserv.client;
-
 /*
- * Copyright 2000-2009 by AO Industries, Inc.,
+ * Copyright 2000-2009, 2014 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
-import com.aoindustries.io.*;
-import java.io.*;
-import java.sql.*;
-import java.util.*;
+package com.aoindustries.aoserv.client;
+
+import com.aoindustries.io.CompressedDataInputStream;
+import com.aoindustries.io.CompressedDataOutputStream;
+import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Each <code>LinuxGroup</code> may be accessed by any number
@@ -21,101 +24,108 @@ import java.util.*;
  * @see  LinuxAccount
  * @see  LinuxGroup
  *
- * @version  1.0a
- *
  * @author  AO Industries, Inc.
  */
 final public class LinuxGroupAccount extends CachedObjectIntegerKey<LinuxGroupAccount> implements Removable {
 
-    static final int COLUMN_PKEY=0;
-    static final String COLUMN_GROUP_NAME_name = "group_name";
-    static final String COLUMN_USERNAME_name = "username";
+	static final int COLUMN_PKEY=0;
+	static final String COLUMN_GROUP_NAME_name = "group_name";
+	static final String COLUMN_USERNAME_name = "username";
 
-    /**
-     * The maximum number of groups allowed for one account.
-     */
-    public static final int MAX_GROUPS=31;
+	/**
+	 * The maximum number of groups allowed for one account.
+	 * 
+	 * <pre>/usr/include/linux/limits.h:#define NGROUPS_MAX    65536</pre>
+	 */
+	public static final int MAX_GROUPS = 65536;
 
-    String group_name;
-    String username;
-    boolean is_primary;
+	String group_name;
+	String username;
+	boolean is_primary;
 
-    Object getColumnImpl(int i) {
-        switch(i) {
-            case COLUMN_PKEY: return Integer.valueOf(pkey);
-            case 1: return group_name;
-            case 2: return username;
-            case 3: return is_primary?Boolean.TRUE:Boolean.FALSE;
-            default: throw new IllegalArgumentException("Invalid index: "+i);
-        }
-    }
+	@Override
+	Object getColumnImpl(int i) {
+		switch(i) {
+			case COLUMN_PKEY: return Integer.valueOf(pkey);
+			case 1: return group_name;
+			case 2: return username;
+			case 3: return is_primary?Boolean.TRUE:Boolean.FALSE;
+			default: throw new IllegalArgumentException("Invalid index: "+i);
+		}
+	}
 
-    public LinuxAccount getLinuxAccount() throws SQLException, IOException {
-        LinuxAccount usernameObject = table.connector.getUsernames().get(username).getLinuxAccount();
-        if (usernameObject == null) throw new SQLException("Unable to find LinuxAccount: " + username);
-        return usernameObject;
-    }
+	public LinuxAccount getLinuxAccount() throws SQLException, IOException {
+		LinuxAccount usernameObject = table.connector.getUsernames().get(username).getLinuxAccount();
+		if (usernameObject == null) throw new SQLException("Unable to find LinuxAccount: " + username);
+		return usernameObject;
+	}
 
-    public LinuxGroup getLinuxGroup() throws SQLException, IOException {
-        LinuxGroup groupNameObject = table.connector.getLinuxGroups().get(group_name);
-        if (groupNameObject == null) throw new SQLException("Unable to find LinuxGroup: " + group_name);
-        return groupNameObject;
-    }
+	public LinuxGroup getLinuxGroup() throws SQLException, IOException {
+		LinuxGroup groupNameObject = table.connector.getLinuxGroups().get(group_name);
+		if (groupNameObject == null) throw new SQLException("Unable to find LinuxGroup: " + group_name);
+		return groupNameObject;
+	}
 
-    public SchemaTable.TableID getTableID() {
-        return SchemaTable.TableID.LINUX_GROUP_ACCOUNTS;
-    }
+	@Override
+	public SchemaTable.TableID getTableID() {
+		return SchemaTable.TableID.LINUX_GROUP_ACCOUNTS;
+	}
 
-    public void init(ResultSet result) throws SQLException {
-        pkey = result.getInt(1);
-        group_name = result.getString(2);
-        username = result.getString(3);
-        is_primary = result.getBoolean(4);
-    }
+	@Override
+	public void init(ResultSet result) throws SQLException {
+		pkey = result.getInt(1);
+		group_name = result.getString(2);
+		username = result.getString(3);
+		is_primary = result.getBoolean(4);
+	}
 
-    public boolean isPrimary() {
-        return is_primary;
-    }
+	public boolean isPrimary() {
+		return is_primary;
+	}
 
-    public void read(CompressedDataInputStream in) throws IOException {
-        pkey=in.readCompressedInt();
-        group_name=in.readUTF().intern();
-        username=in.readUTF().intern();
-        is_primary=in.readBoolean();
-    }
+	@Override
+	public void read(CompressedDataInputStream in) throws IOException {
+		pkey=in.readCompressedInt();
+		group_name=in.readUTF().intern();
+		username=in.readUTF().intern();
+		is_primary=in.readBoolean();
+	}
 
-    public List<CannotRemoveReason> getCannotRemoveReasons() {
-        List<CannotRemoveReason> reasons=new ArrayList<CannotRemoveReason>();
-        if(is_primary) reasons.add(new CannotRemoveReason<LinuxGroupAccount>("Not allowed to drop a primary group", this));
-        return reasons;
-    }
+	@Override
+	public List<CannotRemoveReason> getCannotRemoveReasons() {
+		List<CannotRemoveReason> reasons=new ArrayList<>();
+		if(is_primary) reasons.add(new CannotRemoveReason<>("Not allowed to drop a primary group", this));
+		return reasons;
+	}
 
-    public void remove() throws IOException, SQLException {
-        table.connector.requestUpdateIL(
-            true,
-            AOServProtocol.CommandID.REMOVE,
-            SchemaTable.TableID.LINUX_GROUP_ACCOUNTS,
-            pkey
-        );
-    }
+	@Override
+	public void remove() throws IOException, SQLException {
+		table.connector.requestUpdateIL(
+			true,
+			AOServProtocol.CommandID.REMOVE,
+			SchemaTable.TableID.LINUX_GROUP_ACCOUNTS,
+			pkey
+		);
+	}
 
-    void setAsPrimary() throws IOException, SQLException {
-        table.connector.requestUpdateIL(
-            true,
-            AOServProtocol.CommandID.SET_PRIMARY_LINUX_GROUP_ACCOUNT,
-            pkey
-        );
-    }
+	void setAsPrimary() throws IOException, SQLException {
+		table.connector.requestUpdateIL(
+			true,
+			AOServProtocol.CommandID.SET_PRIMARY_LINUX_GROUP_ACCOUNT,
+			pkey
+		);
+	}
 
-    @Override
-    String toStringImpl() {
-        return group_name+'|'+username+(is_primary?"|p":"|a");
-    }
+	@Override
+	String toStringImpl() {
+		return group_name+'|'+username+(is_primary?"|p":"|a");
+	}
 
-    public void write(CompressedDataOutputStream out, AOServProtocol.Version version) throws IOException {
-        out.writeCompressedInt(pkey);
-        out.writeUTF(group_name);
-        out.writeUTF(username);
-        out.writeBoolean(is_primary);
-    }
+	@Override
+	public void write(CompressedDataOutputStream out, AOServProtocol.Version version) throws IOException {
+		out.writeCompressedInt(pkey);
+		out.writeUTF(group_name);
+		out.writeUTF(username);
+		out.writeBoolean(is_primary);
+	}
 }
