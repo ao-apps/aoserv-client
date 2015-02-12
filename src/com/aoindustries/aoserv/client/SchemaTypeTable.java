1c91259a6a153db7a408a0c7b2511377acffec81
Dan Armstrong <dan@aoindustries.com>
2015-02-12 05:05:30 +0000

    No longer running on mixed HTTP/HTTPS.  Production sites all HTTPS-only now.

--- src/com/aoindustries/aoserv/client/SchemaTypeTable.java
+++ src/com/aoindustries/aoserv/client/SchemaTypeTable.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2001-2013 by AO Industries, Inc.,
+ * Copyright 2001-2013, 2015 by AO Industries, Inc.,
  * 7262 Bull Pen Cir, Mobile, Alabama, 36695, U.S.A.
  * All rights reserved.
  */
@@ -17,72 +17,74 @@ import java.util.List;
  */
 final public class SchemaTypeTable extends GlobalTableIntegerKey<SchemaType> {
 
-    SchemaTypeTable(AOServConnector connector) {
-	super(connector, SchemaType.class);
-    }
+	SchemaTypeTable(AOServConnector connector) {
+		super(connector, SchemaType.class);
+	}
 
-    @Override
-    OrderBy[] getDefaultOrderBy() {
-        return null;
-    }
+	@Override
+	OrderBy[] getDefaultOrderBy() {
+		return null;
+	}
 
-    /*
-    @Override
-    protected int getMaxConnectionsPerThread() {
-        return 2;
-    }*/
+	/*
+	@Override
+	protected int getMaxConnectionsPerThread() {
+		return 2;
+	}*/
 
-    /**
-     * Supports both Integer (num) and String (type) keys.
-     */
-    @Override
-    public SchemaType get(Object pkey) throws IOException, SQLException {
-        if(pkey instanceof Integer) return get(((Integer)pkey).intValue());
-        else if(pkey instanceof String) return get((String)pkey);
-        else throw new IllegalArgumentException("Must be an Integer or a String");
-    }
+	/**
+	 * Supports both Integer (num) and String (type) keys.
+	 */
+	@Override
+	public SchemaType get(Object pkey) throws IOException, SQLException {
+		if(pkey instanceof Integer) return get(((Integer)pkey).intValue());
+		else if(pkey instanceof String) return get((String)pkey);
+		else throw new IllegalArgumentException("Must be an Integer or a String");
+	}
 
-    public SchemaType get(int num) throws IOException, SQLException {
-        return getRows().get(num);
-    }
+	@Override
+	public SchemaType get(int num) throws IOException, SQLException {
+		return getRows().get(num);
+	}
 
-    public SchemaType get(String type) throws IOException, SQLException {
-        return getUniqueRow(SchemaType.COLUMN_TYPE, type);
-    }
+	public SchemaType get(String type) throws IOException, SQLException {
+		return getUniqueRow(SchemaType.COLUMN_TYPE, type);
+	}
 
-    public SchemaTable.TableID getTableID() {
-        return SchemaTable.TableID.SCHEMA_TYPES;
-    }
+	@Override
+	public SchemaTable.TableID getTableID() {
+		return SchemaTable.TableID.SCHEMA_TYPES;
+	}
 
-    public <K,T extends AOServObject<K,T>> void sort(
+	public <K,T extends AOServObject<K,T>> void sort(
 		ComparisonSortAlgorithm<? super T> sortAlgorithm,
 		T[] list,
 		SQLExpression[] sortExpressions,
 		boolean[] sortOrders
 	) {
-        sortAlgorithm.sort(
-            list,
-            new SQLComparator<T>(
-                connector,
-                sortExpressions,
-                sortOrders
-            )
-        );
-    }
+		sortAlgorithm.sort(
+			list,
+			new SQLComparator<T>(
+				connector,
+				sortExpressions,
+				sortOrders
+			)
+		);
+	}
 
-    public <K,T extends AOServObject<K,T>> void sort(
+	public <K,T extends AOServObject<K,T>> void sort(
 		ComparisonSortAlgorithm<? super T> sortAlgorithm,
 		List<T> list,
 		SQLExpression[] sortExpressions,
 		boolean[] sortOrders
 	) {
-        sortAlgorithm.sort(
-            list,
-            new SQLComparator<T>(
-                connector,
-                sortExpressions,
-                sortOrders
-            )
-        );
-    }
+		sortAlgorithm.sort(
+			list,
+			new SQLComparator<T>(
+				connector,
+				sortExpressions,
+				sortOrders
+			)
+		);
+	}
 }
