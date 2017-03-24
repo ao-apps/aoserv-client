c035f0d07ec9c57ed0812621edae1d65cab0711c
AO Industries, Inc. <support@aoindustries.com>
2017-03-24 12:16:37 -0500

    Additional buffer statistics.

--- src/main/java/com/aoindustries/aoserv/client/MasterServerStat.java
+++ src/main/java/com/aoindustries/aoserv/client/MasterServerStat.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-client - Java client for the AOServ platform.
- * Copyright (C) 2001-2013, 2015, 2016  AO Industries, Inc.
+ * Copyright (C) 2001-2013, 2015, 2016, 2017  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -38,42 +38,47 @@ import java.sql.SQLException;
 final public class MasterServerStat extends AOServObject<String,MasterServerStat> implements SingleTableObject<String,MasterServerStat> {
 
 	public static final String
-		BYTE_ARRAY_CACHE_CREATES="byte_array_cache_creates",
-		BYTE_ARRAY_CACHE_USES="byte_array_cache_uses",
-		CHAR_ARRAY_CACHE_CREATES="char_array_cache_creates",
+		BYTE_ARRAY_CACHE_CREATES = "byte_array_cache_creates",
+		BYTE_ARRAY_CACHE_USES = "byte_array_cache_uses",
+		BYTE_ARRAY_CACHE_ZERO_FILLS = "byte_array_cache_zero_fills",
+		BYTE_ARRAY_CACHE_COLLECTED = "byte_array_cache_collected",
+		CHAR_ARRAY_CACHE_CREATES = "char_array_cache_creates",
 		CHAR_ARRAY_CACHE_USES="char_array_cache_uses",
-		DAEMON_CONCURRENCY="daemon_concurrency",
-		DAEMON_CONNECTIONS="daemon_connections",
-		DAEMON_CONNECTS="daemon_connects",
-		DAEMON_COUNT="daemon_count",
-		DAEMON_DOWN_COUNT="daemon_down_count",
-		DAEMON_MAX_CONCURRENCY="daemon_max_concurrency",
-		DAEMON_POOL_SIZE="daemon_pool_size",
-		DAEMON_TOTAL_TIME="daemon_total_time",
-		DAEMON_TRANSACTIONS="daemon_transactions",
-		DB_CONCURRENCY="db_concurrency",
-		DB_CONNECTIONS="db_connections",
-		DB_CONNECTS="db_connects",
-		DB_MAX_CONCURRENCY="db_max_concurrency",
-		DB_POOL_SIZE="db_pool_size",
-		DB_TOTAL_TIME="db_total_time",
-		DB_TRANSACTIONS="db_transactions",
-		ENTROPY_AVAIL="entropy_avail",
-		ENTROPY_POOLSIZE="entropy_poolsize",
-		ENTROPY_READ_BYTES="entropy_read_bytes",
-		ENTROPY_READ_COUNT="entropy_read_count",
-		ENTROPY_WRITE_BYTES="entropy_write_bytes",
-		ENTROPY_WRITE_COUNT="entropy_write_count",
-		MEMORY_FREE="memory_free",
-		MEMORY_TOTAL="memory_total",
-		PROTOCOL_VERSION="protocol_version",
-		REQUEST_CONCURRENCY="request_concurrency",
-		REQUEST_CONNECTIONS="request_connections",
-		REQUEST_MAX_CONCURRENCY="request_max_concurrency",
-		REQUEST_TOTAL_TIME="request_total_time",
-		REQUEST_TRANSACTIONS="request_transactions",
-		THREAD_COUNT="thread_count",
-		UPTIME="uptime"
+		CHAR_ARRAY_CACHE_ZERO_FILLS = "char_array_cache_zero_fills",
+		CHAR_ARRAY_CACHE_COLLECTED = "char_array_cache_collected",
+		DAEMON_CONCURRENCY = "daemon_concurrency",
+		DAEMON_CONNECTIONS = "daemon_connections",
+		DAEMON_CONNECTS = "daemon_connects",
+		DAEMON_COUNT = "daemon_count",
+		DAEMON_DOWN_COUNT = "daemon_down_count",
+		DAEMON_MAX_CONCURRENCY = "daemon_max_concurrency",
+		DAEMON_POOL_SIZE = "daemon_pool_size",
+		DAEMON_TOTAL_TIME = "daemon_total_time",
+		DAEMON_TRANSACTIONS = "daemon_transactions",
+		DB_CONCURRENCY = "db_concurrency",
+		DB_CONNECTIONS = "db_connections",
+		DB_CONNECTS = "db_connects",
+		DB_MAX_CONCURRENCY = "db_max_concurrency",
+		DB_POOL_SIZE = "db_pool_size",
+		DB_TOTAL_TIME = "db_total_time",
+		DB_TRANSACTIONS = "db_transactions",
+		ENTROPY_AVAIL = "entropy_avail",
+		ENTROPY_POOLSIZE = "entropy_poolsize",
+		ENTROPY_READ_BYTES = "entropy_read_bytes",
+		ENTROPY_READ_COUNT = "entropy_read_count",
+		ENTROPY_WRITE_BYTES = "entropy_write_bytes",
+		ENTROPY_WRITE_COUNT = "entropy_write_count",
+		MEMORY_FREE = "memory_free",
+		MEMORY_TOTAL = "memory_total",
+		// TODO: Coalesce version ranges
+		PROTOCOL_VERSION = "protocol_version",
+		REQUEST_CONCURRENCY = "request_concurrency",
+		REQUEST_CONNECTIONS = "request_connections",
+		REQUEST_MAX_CONCURRENCY = "request_max_concurrency",
+		REQUEST_TOTAL_TIME = "request_total_time",
+		REQUEST_TRANSACTIONS = "request_transactions",
+		THREAD_COUNT = "thread_count",
+		UPTIME = "uptime"
 	;
 
 	String name;
