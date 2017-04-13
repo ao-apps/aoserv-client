2de6c889afdd4b5b588fb708dff10a38a5b5c13c
AO Industries, Inc. <support@aoindustries.com>
2017-04-13 14:06:03 -0500

    Capitalized "AOServ Platform" in project description.

--- src/main/java/com/aoindustries/aoserv/client/Technology.java
+++ src/main/java/com/aoindustries/aoserv/client/Technology.java
@@ -1,6 +1,6 @@
 /*
- * aoserv-client - Java client for the AOServ platform.
- * Copyright (C) 2000-2009, 2016  AO Industries, Inc.
+ * aoserv-client - Java client for the AOServ Platform.
+ * Copyright (C) 2000-2009, 2016, 2017  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
