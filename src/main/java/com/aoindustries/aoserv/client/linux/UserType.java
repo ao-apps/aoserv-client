520d63fb4dff93ca5025e26819f8bf21856c6f95
AO Industries, Inc. <support@aoindustries.com>
2019-07-07 23:59:54 -0500

    New canPostgresIdent() methods to control which Linux accounts can authentication to PostgreSQL via "peer" and "ident".

--- src/main/java/com/aoindustries/aoserv/client/linux/UserType.java
+++ src/main/java/com/aoindustries/aoserv/client/linux/UserType.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-client - Java client for the AOServ Platform.
- * Copyright (C) 2000-2009, 2016, 2017, 2018  AO Industries, Inc.
+ * Copyright (C) 2000-2009, 2016, 2017, 2018, 2019  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -175,6 +175,16 @@ final public class UserType extends GlobalObjectStringKey<UserType> {
 		pkey = result.getString(1);
 		description = result.getString(2);
 		is_email = result.getBoolean(3);
+	}
+
+	public boolean canPostgresIdent() {
+		return canPostgresIdent(pkey);
+	}
+
+	public static boolean canPostgresIdent(String type) {
+		return
+			APPLICATION.equals(type)
+			|| USER.equals(type);
 	}
 
 	public boolean isAllowedShell(Shell shell) throws SQLException {
