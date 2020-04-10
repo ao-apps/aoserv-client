2ee5b717d823a2ee21756e56909d6d109b9ac635
AO Industries, Inc <support@aoindustries.com>
2020-04-10 15:50:33 -0500

    Comma-separated adjacent years in top comment

--- src/main/java/com/aoindustries/aoserv/client/dto/AccountName.java
+++ src/main/java/com/aoindustries/aoserv/client/dto/AccountName.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-client - Java client for the AOServ Platform.
- * Copyright (C) 2010-2011, 2016, 2017, 2018  AO Industries, Inc.
+ * Copyright (C) 2010, 2011, 2016, 2017, 2018  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
