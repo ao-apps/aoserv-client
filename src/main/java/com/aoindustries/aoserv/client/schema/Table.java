df33449c53737c15bb0bbc8ff3ec04ed06cd38fb
AO Industries, Inc <support@aoindustries.com>
2021-02-16 10:40:13 -0600

    Using Boolean.toString(boolean)

--- src/main/java/com/aoindustries/aoserv/client/schema/Table.java
+++ src/main/java/com/aoindustries/aoserv/client/schema/Table.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-client - Java client for the AOServ Platform.
- * Copyright (C) 2001-2013, 2015, 2016, 2017, 2018, 2019, 2020  AO Industries, Inc.
+ * Copyright (C) 2001-2013, 2015, 2016, 2017, 2018, 2019, 2020, 2021  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -462,8 +462,8 @@ final public class Table extends GlobalObjectIntegerKey<Table> {
 			rows.add(new Object[] {
 				column.getName(),
 				column.getType(connector).getName(),
-				column.isNullable() ? "true" : "false",
-				column.isUnique() ? "true" : "false",
+				Boolean.toString(column.isNullable()),
+				Boolean.toString(column.isUnique()),
 				formatForeignKeys(connector, column.getReferences(connector), true),
 				formatForeignKeys(connector, column.getReferencedBy(connector), false),
 				column.getDescription()
