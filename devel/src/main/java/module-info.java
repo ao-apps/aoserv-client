e0d04dde6314ef0110a0c35ab97c9be7f923a705
AO Industries, Inc <support@aoindustries.com>
2022-04-11 12:43:15 -0500

    Marked resource bundles ThreadSafe

--- devel/src/main/java/module-info.java
+++ devel/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 /*
  * aoserv-client - Java client for the AOServ Platform.
- * Copyright (C) 2021  AO Industries, Inc.
+ * Copyright (C) 2021, 2022  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -35,6 +35,7 @@ module com.aoindustries.aoserv.client.devel {
 	exports com.aoindustries.aoserv.client.ticket.i18n;
 	// Direct
 	requires com.aoapps.hodgepodge; // <groupId>com.aoapps</groupId><artifactId>ao-hodgepodge</artifactId>
+	requires static jsr305; // <groupId>com.google.code.findbugs</groupId><artifactId>jsr305</artifactId>
 	// Java SE
 	requires java.logging;
 }
